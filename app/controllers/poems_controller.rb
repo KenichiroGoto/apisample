22e51cc8a31d08d66593ca65a941a1bc57ba09f4
後藤研一郎 <gotokenichiro@gotoukenichirounoMacBook-Pro.local>
2017-04-01 10:55:03 +0900

    add show action

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -4,5 +4,11 @@ class PoemsController < ApplicationController
     @poems = Poem.all
     render json: @poems
   end
-  
+
+  # Add show action for normal gate of dive14_2
+  def show
+    @poem = Poem.find(params[:id])
+    render json: @poem
+  end
+
 end
